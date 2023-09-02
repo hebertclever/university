47e9baecc728393b3f40e6ba4aed06bc306f500e
hebertclever <hebertclever@gmail.com>
2023-09-02 19:36:08 -0300

    sidebar2 no inicio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.php"], // Especifica quais arquivos devem ser analisados pelo Tailwind
-  darkMode: 'class', // Configuração válida para o modo escuro
+  content: ["./src/**/*.php"], 
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        'primaria': '#2450A3',
+        'primaria': '#2A3965',
         'secundaria': '#1194CF',
-        'background': '#002D82',
+        'background': '#F2F6FF',
+        'background-2': '#DEE9F7',
         'text-1': '#1A2947',
-        'text-2': '#8C9DC6',
-        'text-hover': '#373B46',
-        'icon-1': '#B7C6EB',
-        'icon-2': '#5F40F9',
+        'text-2': '#8C9DC6',        
+        'icon-1': '#2A3965',
+        'icon-2': '#BEB8FF',
         'category-bg1': '#DEE9F7',
         'category-bg2': '#9184FF',
       },
